e7727bea54864e41715ea1aa88c0090846294f8d
Admin <shirimas@gmail.com>
2018-01-10 23:46:13 +0300

    Modification of the sqlserver credentials for hosting

--- config/database.php
+++ config/database.php
@@ -39,7 +39,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -52,9 +52,9 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],
+        ],*/
 
-        /*'mysql' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'sql2.freesqldatabase.com'),
             'port' => env('DB_PORT', '3306'),
@@ -67,7 +67,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-        ],*/
+        ],
 
         'tracker' => [
             'driver' => 'mysql',
